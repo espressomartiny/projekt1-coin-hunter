7e8dcc8a4e6683d4b988b17c5905a3d58ae0e325
Martina Vokáčová <martinavokacova@gmail.com>
2022-03-06 14:04:42 +0100

    změna obrázku při stisknutí klávesy

--- main.js
+++ main.js
@@ -5,5 +5,25 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 }
 */
 
-
 // sem začni psát svůj program
+
+let panacek = document.querySelector("#panacek");
+panacek.style.left = "500px";
+panacek.style.top = "100px";
+
+function stiskKlavesy(udalost) {
+  console.log(udalost.key, udalost.keyCode);
+
+  if (udalost.keyCode === 37) {
+    panacek.src = src = "obrazky/panacek-vlevo.png";
+  }
+  if (udalost.keyCode === 38) {
+    panacek.src = src = "obrazky/panacek-nahoru.png";
+  }
+  if (udalost.keyCode === 39) {
+    panacek.src = src = "obrazky/panacek-vpravo.png";
+  }
+  if (udalost.keyCode === 40) {
+    panacek.src = src = "obrazky/panacek.png";
+  }
+}
