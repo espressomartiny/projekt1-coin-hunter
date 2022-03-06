dfabfd4ee4deb412b63b903373343bae64d5b314
Martina Vokáčová <martinavokacova@gmail.com>
2022-03-06 14:48:28 +0100

    přidání pohybu

--- main.js
+++ main.js
@@ -10,20 +10,27 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 let panacek = document.querySelector("#panacek");
 panacek.style.left = "500px";
 panacek.style.top = "100px";
+let i = 1;
 
 function stiskKlavesy(udalost) {
   console.log(udalost.key, udalost.keyCode);
+  	let poziceL = panacek.style.left;
+	let poziceT = panacek.style.top;
 
   if (udalost.keyCode === 37) {
     panacek.src = src = "obrazky/panacek-vlevo.png";
+	panacek.style.left = parseInt(poziceL) - i++ + "px";
   }
   if (udalost.keyCode === 38) {
     panacek.src = src = "obrazky/panacek-nahoru.png";
+	panacek.style.top = parseInt(poziceT) - i++ + "px";
   }
   if (udalost.keyCode === 39) {
     panacek.src = src = "obrazky/panacek-vpravo.png";
+	panacek.style.left = parseInt(poziceL) + i++ + "px";
   }
   if (udalost.keyCode === 40) {
     panacek.src = src = "obrazky/panacek.png";
+	panacek.style.top = parseInt(poziceT) + i++ + "px";
   }
 }
